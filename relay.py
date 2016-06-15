b84982f73ebac6a67daacaf669e0ad4fa97f2bf4
Way, No <noway@2ch.hk>
2016-06-15 18:29:56 +1200

    PEP8

--- relay.py
+++ relay.py
@@ -9,10 +9,12 @@ loop = None
 RELAY_WAIT_PORT = 8001
 RELAY_ACTION_PORT = 8002
 
+
 def accept_client(client_reader, client_writer):
 
     log.info("New Connection")
     task = asyncio.Task(handle_client(client_reader, client_writer))
+
 
 class EchoClient(asyncio.Protocol):
 
@@ -21,23 +23,24 @@ class EchoClient(asyncio.Protocol):
 
     def connection_made(self, transport):
         self.transport = transport
-        log.info('connection made:', self)
+        log.info('Connection made:', self)
 
     def data_received(self, data):
         self.writer.write(data)
 
     def eof_received(self):
-        log.info("Got EOF from server connection")
+        log.info("Got EOF from server")
 
     def connection_lost(self, exc):
-        log.info("Got close from server connection")
+        log.info("Got close from server")
         try:
             self.writer.write_eof()
         except Exception:
             pass
 
+
 def conn_lost(tr):
-    log.info("Got close from client connection")
+    log.info("Got close from client")
     try:
         tr.write_eof()
     except Exception:
@@ -48,6 +51,7 @@ async def handle_client(client_reader, client_writer):
     data = None
     port = None
     protocol = None
+    client_protocol = client_writer._protocol
 
     try:
         data = await asyncio.wait_for(client_reader.read(1), timeout=1.0)
@@ -56,18 +60,21 @@ async def handle_client(client_reader, client_writer):
 
     if data is None:
         log.info("opening RELAY_WAIT_PORT")
-        port = RELAY_WAIT_PORT;
+        port = RELAY_WAIT_PORT
     else:
         log.info("opening RELAY_ACTION_PORT")
         port = RELAY_ACTION_PORT
 
     try:
-        _, protocol = await asyncio.ensure_future(loop.create_connection(EchoClient, "127.0.0.1", port))
+        _, protocol = await asyncio.ensure_future(
+            loop.create_connection(EchoClient, "127.0.0.1", port))
+
         protocol.writer = client_writer
+        client_protocol.other = protocol
+
         if data is not None:
             protocol.transport.write(data)
 
-        client_writer._protocol.other = protocol
     except Exception:
         pass
 
@@ -76,9 +83,10 @@ async def handle_client(client_reader, client_writer):
         client_writer.write_eof()
         return
 
-    client_writer._protocol.data_received = lambda data: protocol.transport.write(data)
-    client_writer._protocol.eof_received = lambda: log.info("Got EOF from client connection")
-    client_writer._protocol.connection_lost = lambda exc: conn_lost(protocol.transport)
+    client_protocol.data_received = lambda data: protocol.transport.write(data)
+    client_protocol.eof_received = lambda: log.info("Got EOF from client")
+    client_protocol.connection_lost = lambda exc: conn_lost(protocol.transport)
+
 
 def main():
     global loop
@@ -86,6 +94,7 @@ def main():
     f = asyncio.start_server(accept_client, host=None, port=8000)
     loop.run_until_complete(f)
     loop.run_forever()
+
 
 if __name__ == '__main__':
     log = logging.getLogger("")
